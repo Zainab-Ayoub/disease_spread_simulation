b8c331b228575afc428130e7baa74455192afeee
Zainab-Ayoub <zainabzafar365@gmail.com>
2025-10-16 19:01:11 +0500

    paths

--- src/data/flunet_prepare.py
+++ src/data/flunet_prepare.py
@@ -3,3 +3,13 @@ from pathlib import Path # pathlib to handle file paths
 import numpy as np 
 import pandas as pd
 
+try:
+    import pyarrow
+    HAS_PARQUET = True
+except:
+    HAS_PARQUEST = False 
+
+raw_dir = Path('data/raw/flunet_weekly.csv')
+out_dir = Path('data/processed')
+out_dir.mkdir(parents=True, exist_ok = True)
+
